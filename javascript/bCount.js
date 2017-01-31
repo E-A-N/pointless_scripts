31962500714f2f0d3ba9b54a61d7fde41b5845e4
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-01-31 12:07:11 -0800

    Update bCount.js

--- javascript/bCount.js
+++ javascript/bCount.js
@@ -1,5 +1,7 @@
 function bCount(stri,choice){
-
+    /*
+        This function counts the amount of a given character inside an array
+    */
     var trueOrFalse = (x)=>(x===choice);
     //convert true to 1 or false to 0
     var zeroOne = function(x){return x === true ? 1 : 0};
