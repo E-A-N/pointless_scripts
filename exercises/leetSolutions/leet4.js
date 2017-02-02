fb90fb4614c5b72d6bb7e1c82b559e3ad798e799
Cyborg_Ean <ean.director@gmail.com>
2017-02-02 10:22:35 -0800

    working on flattening an array

--- exercises/leetSolutions/leet4.js
+++ exercises/leetSolutions/leet4.js
@@ -20,12 +20,38 @@ note: the median of odd lists are (N-length + 1) / 2
 
 function medians(nums1,nums2){
     let flat=[];
-    for(let x = 0; x < nums1.length; x++){
-        for(let y = 0; y < nums2.legnth; y++){
-            if (x >= y){
-                flat[x] = nums1[x];
-                
+    let ray1;
+    let ray2;
+
+    //Choose which array to search first
+    if(nums1[0] <= nums2[0]){
+        ray1 = nums1;
+        ray2 = nums2;
+    }
+    else{
+        ray1 = nums2;
+        ray2 = nums1;
+    }
+
+    //Assign lowest element to flattened array
+    flat.push(ray1[0]);
+
+    let leastVal = 0;
+    for(let x = 1; x < ray1.length; x++){
+        for(let y = 0 + leastVal; y < ray2.length; y++){
+            if (ray1[x] >= ray2[y]){
+                flat.push(ray2[y]);
+            }
+            else {
+                flat.push(ray1[x]);
+                leastVal = y;
+                break;
             }
         }
     }
+    return flat;
 }
+
+var r1 = [1,4,7,9,9];
+var r2 = [0,2,4,7,8,9];
+var f = medians(r1,r2);
