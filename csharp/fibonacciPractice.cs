2383c0097b91906ba48d32d952ac1eba2fccee7a
Cyborg_Ean <ean.director@gmail.com>
2017-02-21 09:28:51 -0800

    practicing fib sequence

---
+++ csharp/fibonacciPractice.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Application
+{
+	public class Fibonacci {
+		public int[] calcs;
+
+		private void getFibos(ref int[] num){
+			//calucalte Fibonacci numbers decreasing from N
+		}
+
+		public static void main(){
+			Fibonacci fibo = new Fibonacci();
+			
+		}
+	}
+}
