5ababf49df23f399c8263e89045b808358ecabb1
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-02-20 23:15:41 -0800

    fixed solution
    Need to practice queueing in place next, this queue creates a copy of an array

--- csharp/queue.cs
+++ csharp/queue.cs
@@ -1,23 +1,25 @@
 using System;
 class queue {
   
-  public void que(out int[] ray, int size){
-    ray = new int[size];
-    int len = ray.Length;
-    int placeHold = ray[len - 1];
-    
-    //Use decrementation
-    for (int i = len - 2; i > 0; i--){
-        ray[i + 1] = ray[i];
+    //returns copy of array
+    public int[] que(int[] ray, int size){
+        int len = size;
+        int placeHold = ray[len - 1];
+        
+        //Shift array starting from the end
+        for (int i = len - 2; i > -1; i--){
+            ray[i + 1] = ray[i];
+        }
+        ray[0] = placeHold;
+        return ray;
     }
-    ray[0] = placeHold;
-  }
-  
-  public static void Main (string[] args) {
-    queue rayray = new queue();
-    int[] xay = {1,2,3};
-    rayray.que(xay,xay.Length);
-    Console.WriteLine ("{0}",xay[0]);
-  }
+    
+    public static void Main (string[] args) {
+        queue xVar = new queue();
+        
+        int[] xay = {1,2,3,4};
+        
+        xay = xVar.que(xay,xay.Length);
+        Console.WriteLine ("{0}",string.Join(",",xay));
+    }
 }
-      
