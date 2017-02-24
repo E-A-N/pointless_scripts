d4e6e85a1e8c36f2e6fb71c0d3a429ff1e3c662f
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-02-23 18:07:53 -0800

    more progress on exercise 6

--- csharp/elementaryProblems.cs
+++ csharp/elementaryProblems.cs
@@ -166,7 +166,8 @@ class MainClass {
 }
       
 /*
-  6. Write a program that asks the user for a number n and gives him the possibility to choose between computing the sum and computing the product of 1,…,n.
+  6. Write a program that asks the user for a number n and gives him the 
+     possibility to choose between computing the sum and computing the product of 1,…,n.
 */
 
 
@@ -176,9 +177,10 @@ class nameAndNum {
     //attributes
     private string name;
     private int number;
+    private int decision;
     
-    //getters
-    private void getName(){
+    //setters
+    private void setName(){
         string name;
         Console.WriteLine("Herro Robo, what is your name?: ");
         name = Console.ReadLine();
@@ -186,21 +188,53 @@ class nameAndNum {
         
         this.name = name;
     }
-    
-    private void getNumber(){
+    private void setNumber(){
         int num;
-        Console.WriteLine("Herro Robo, what is your name?: ");
+        Console.WriteLine("Herro {0}, what\'s your favorite number?: ",this.name);
         num = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("It\'s nice to meet you, {0}",num);
+        Console.WriteLine("{0} is your number, {1}!",num,this.name);
         
         this.number = num;
+    }
+    private void setDecision(){
+        int decision;
+        string msg = "Press 1 to get sums to {0}: \nPress 2 to get product to {0}: \nPress 0 to quit!";
+        Console.WriteLine(msg,this.number);
+        decision = Convert.ToInt32(Console.ReadLine());
+        
+        this.reiterateDecision(decision);
+        this.decision = decision;
+    }
+    
+    //Behaviors
+    private void reiterateDecision(decision){
+        string msg;
+        switch(decision){
+          
+            case 1:
+                msg = "You wish to add all of the numbers from 0  to {0}! Cool {1}!";
+                Console.WriteLine(msg,this.number,this.name);
+                break;
+                
+            case 2:
+                msg = "You wish to multiply all of the numbers from 0 to {0}! That\'s pretty awesome {1}!";
+                Console.WriteLine(msg,this.number,this.name);
+                break;
+                
+            case 0:
+                msg = "Sorry {0}, that\'s unfortunate!";
+                Console.WriteLine(msg,this.name);
+                break;
+          
+        }
     }
     
     public static void Main (string[] args) {
         nameAndNum app; //initialize program
         
         app = new nameAndNum(); //instantiate program
-        app.getName();
-        Console.WriteLine(app.name);
+        app.setName();
+        app.setNumber();
+        app.setDecision();
     }
 }
