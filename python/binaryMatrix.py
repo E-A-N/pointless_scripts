19d2aad2bd64c2466268291e6915cafd8290a23e
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-04-04 15:04:07 -0700

    Added constants for winning condititions

--- python/binaryMatrix.py
+++ python/binaryMatrix.py
@@ -1,4 +1,7 @@
+
 ticTacToe   = 0
+
+#Bit equivalent to plane coordinates (tictactoe matrix table)
 topLeft     = 0b100000000
 topMid      = 0b10000000
 topRight    = 0b1000000
@@ -9,10 +12,15 @@ bottomLeft  = 0b100
 bottomMid   = 0b10
 bottomRight = 0b1
 
-ticTacToe += topLeft
-ticTacToe += topMid
-ticTacToe += topRight
-ticTacToe += bottomRight
+#Winning Conditions
+topHorWin     = 0b111000000
+midHorWin     = 0b000111000
+bottomHorWin  = 0b000000111
+leftVertWin   = 0b100100100
+midVertWin    = 0b010010010
+rightVertWin  = 0b001001001
+leftDiag      = 0b100010001
+rightDiag     = 0b001010100
 
 def bitCount(bit):
     count = 0
