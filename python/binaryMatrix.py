6f21ae32277f05b10968e04fd93e15b398281c6f
Cyborg_Ean <ean.director@gmail.com>
2017-04-04 13:22:54 -0700

    quick update

--- python/binaryMatrix.py
+++ python/binaryMatrix.py
@@ -9,8 +9,18 @@ ticTacToe += topMid
 ticTacToe += topRight
 ticTacToe += bottomRight
 
+def bitCount(bit):
+    count = 0
+    while (bit > 1):
+        bit/=2
+        count += 1
+    return count
+
 def printTrix(data):
     for i in data:
         print(i,end='')
 
+
+#printTrix(ticTacToe)
 print(bin(ticTacToe))
+print(bitCount(ticTacToe))
