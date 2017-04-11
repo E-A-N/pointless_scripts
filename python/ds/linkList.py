bcadcd83384ae5e81a11bc64490e349b35057969
Cyborg_Ean <ean.director@gmail.com>
2017-04-10 21:32:54 -0700

    implementing program loop and added delete method

--- python/ds/linkList.py
+++ python/ds/linkList.py
@@ -17,6 +17,14 @@ class LinkedList:
             self.currentNode = self.currentNode.nextNode
             return
 
+    def deleteCurrentNode(self):
+            hereNode = self.currentNode
+            prevNode = self.prevNode
+            nextNode = self.nextNode
+            if (nextNo != None):
+                nextNo.prevNode = prev
+            if (prevNo != None):
+                prev.nextNode = nextNode
 
 
 
@@ -26,3 +34,22 @@ ll.insertNode("Luffy! =D")
 print(ll.currentNode.name)
 print(ll.currentNode.prevNode.nextNode.prevNode.nextNode.prevNode.name)
 #LOL
+
+
+
+def runner():
+    dataIn = input("Enter d to deleten\ni to insert\nq to quit")
+    result = 1
+    if (dataIn[0].lower() == 'i'):
+        node = input("What is the name of the node: ")
+        ll.insertNode(node)
+    elif (dataIn[0].lower() == 'd'):
+        input("Deleting Node...")
+        ll.deleteCurrentNode()
+    else:
+        result = 0
+    return result
+
+while True():
+    if runner() == 0:
+        break
