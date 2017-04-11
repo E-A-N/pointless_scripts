99f98deb27930d27525341a5cf94b1c3bd8b628d
Cyborg_Ean <ean.director@gmail.com>
2017-04-10 17:11:46 -0700

    Link list implemented, can experiment with concepts later

--- python/ds/linkList.py
+++ python/ds/linkList.py
@@ -1,16 +1,28 @@
 class LinkedList:
     def __init__(self):
-        self.headLink = self.Node("tip",True,None)
+        self.headLink = self.Node("tip",True)
+        self.currentNode = self.headLink
 
     class Node:
-        def __init__(self,name,head,nextNode):
+        def __init__(self,name,head, nextNode = None, prevNode = None):
             self.name = name
             self.head = head
             self.nextNode = nextNode
+            self.prevNode = prevNode
 
     def insertNode(self,name):
-        return
+            cur = self.currentNode
+            self.currentNode.nextNode = self.Node(name,False,None, cur)
+            #automatically go to next node
+            self.currentNode = self.currentNode.nextNode
+            return
+
+
 
 
 ll = LinkedList()
-print(ll.headLink.name)
+print(ll.currentNode.name)
+ll.insertNode("Luffy! =D")
+print(ll.currentNode.name)
+print(ll.currentNode.prevNode.nextNode.prevNode.nextNode.prevNode.name)
+#LOL
