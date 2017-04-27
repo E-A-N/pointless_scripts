8eb15d79ed775dd50b0d7ffca79fb84e2051f4cf
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-04-27 10:00:06 -0700

    Create apiDate.js

---
+++ exercises/interviewTests/apiDate.js
@@ -0,0 +1,26 @@
+/*Convert the date to a api friend format
+
+Example: formateDate("5/12/2770") -> '27701205'
+
+*/
+
+function formatDate(userDate) {
+    // format from M/D/YYYY to YYYYMMDD
+    var result;
+    //Break the input into components based off of '/' character
+    var ray = userDate.split('/');
+    //abstract different components of the date
+    var year = ray[2];
+    var month = ray[0];
+    var day = ray[1];
+    
+    //adjust format for single digits
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day
+    //Thank you cohersion! <3
+    
+    //concatentate date components to make result api friendly
+    result = year + month + day;
+    return result;
+  
+}
