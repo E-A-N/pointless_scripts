3128b1023f0d475a487eb2f1b3bb76804ca6f549
Cyborg_Ean <ean.director@gmail.com>
2017-05-01 22:51:30 -0700

    debugging tree traversal

--- dataStructures/findRandNode.js
+++ dataStructures/findRandNode.js
@@ -4,20 +4,22 @@ create a random number from 0 to 500 Use a binary search tree to find this value
 
 var Node = function(){};
 
-Node.prototype.init = function(val,left = null,right = null){
-    this.value = 0;
-    this.leftChild = left;
-    this.rightChild = ritgt;
+Node.prototype.init = function(val,left,right){
+    this.value = val || 0;
+    this.leftChild = left || null;
+    this.rightChild = right || null;
 }
 
 var Tree = function(){};
 
-Tree.prototype.init = function(root){
+Tree.prototype.init = function(rootValue){
     this.root = new Node;
-    this.root.init(root);
+    this.root.init(rootValue);
+    this.generateTree(rootValue, 10);
 }
 
 Tree.prototype.traverse = function(node, call){
+
     //In Order Traversal
     if (node.leftChild){
         call(node.leftChild);
@@ -32,16 +34,27 @@ Tree.prototype.traverse = function(node, call){
     return node.value;
 }
 
-Tree.prototype.generateTree = function(node,max,depth){
+Tree.prototype.generateTree = function(node,depth){
     if (depth > 0){
 
+        var offset = Math.floor(Math.random() * 3) + 1;
         node.leftChild = new Node;
-        node.leftChild.value = node.value - 3;
-        this.generateTree(node.leftChild, node.value, depth - 1);
+        node.leftChild.init();
+        node.leftChild.value = node.value - offset;
+        this.generateTree(node.leftChild, depth - 1);
 
         node.rightChild = new Node;
-        node.rightChild.value = max - depth;
-        this.generateTree(node.rightChild, max, depth - 1);
+        node.rightChild.init();
+        node.rightChild.value = max + offset;
+        this.generateTree(node.rightChild, depth - 1);
     }
     return;
 }
+
+var printNode = function(node){
+    console.log(node.value);
+}
+console.log("Herro Robo! <3");
+var oak = new Tree;
+oak.init(44);
+oak.traverse(oak.root,printNode);
