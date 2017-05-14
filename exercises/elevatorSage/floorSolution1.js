f6a4b9a3127aa99fa7d2dd94fd41f8f16c1ee0fa
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-05-14 01:45:24 -0700

    currently addressing bug for this.center
    it's logging as undefined

--- exercises/elevatorSage/floorSolution1.js
+++ exercises/elevatorSage/floorSolution1.js
@@ -3,56 +3,47 @@
     init: function(elevators, floors) {
 
         //Add custom attributes to elevators
-        //this.setNeWAttributes(elevators);
-        this.center = this.setCenterFloor(floors);
+        //this.setNewAttributes(elevators);
+        //this.center = this.setCenterFloor(floors);
+        this.setCenterFloor(floors);
         this.elevatorEvents(elevators);
 
-        /*
-        var elevator = elevators[0]; // Let's use the first elevator
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            // let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(this.center);
-        });
-
-        elevator.on("floor_button_pressed", function(floorNum) {
-            // Maybe tell the elevator to go to that floor?
-            elevator.goToFloor(floorNum);
-        });
-        */
     },
 
     update: function(dt, elevators, floors) {
        // We normally don't need to do anything here
     },
 
+    center: 0,
     setCenterFloor: function(floors){
         var total = floors.length;
         total = Math.floor(total/2);
-        return total;
+        this.center = total;
+        //return total;
     },
 
-    setNeWAttributes: function(elevators){
+    setNewAttributes: function(elevators){
         var eLen = elevators.length;
         for (var i = 0; i < eLen; i++){
             elevators[i].idle = false;
         }
     },
-    center: 0,
+
     elevatorEvents: function(elevators){
-      var eLen = elevators.length;
-      for (var i = 0; i < eLen; i++) {
-          var elevator = elevators[i];
-          elevator.idle = false;
+        var eLen = elevators.length;
+        for (var i = 0; i < eLen; i++) {
+            var elevator = elevators[i];
+            elevator.idle = false;
 
-          elevator.on("idle",function(){
-            elevator.goToFloor(this.center);
-          });
+            elevator.on("idle",function(){
+              //elevator.goToFloor(this.center);
+              elevator.goToFloor(1);
+            });
 
-          elevator.on("floor_button_pressed", function(floorNum) {
-              // Maybe tell the elevator to go to that floor?
-              elevator.goToFloor(floorNum);
-          });
-      }
+            elevator.on("floor_button_pressed", function(floorNum) {
+                // Maybe tell the elevator to go to that floor?
+                elevator.goToFloor(floorNum);
+            });
+        }
     },
 }
