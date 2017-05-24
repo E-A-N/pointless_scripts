b8bbd5cae47a1f67ba1283ed62c5ad2372662f89
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-05-24 12:36:38 -0700

    learning basics

--- exercises/php/helloRobo.php
+++ exercises/php/helloRobo.php
@@ -1,4 +1,22 @@
 <?php
-  echo "Herro Robo!";
+  //php has no newline, the break tag must be used instead
+  echo 'Herro Robo!<br>';
+
+
+  echo "Herro Robo! + ";
+  echo 5 + 5;
+  echo ", -10!!";
+
+  print('
+      <br>
+      <strong>
+      This is bold!!!
+      </strong>        
+      <br>        
+      This is not!
+      <br>
+  ');
+
+  echo "Can echo do <strong> BOLD </strong> too!?";
 
 ?>
