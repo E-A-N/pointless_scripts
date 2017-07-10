58aaf3c68b5cf67233cd9e574074e0009952c49c
Cyborg_Ean <ean.director@gmail.com>
2017-07-10 12:58:53 -0700

    playing around

---
+++ javascript/randomeStuff.js
@@ -0,0 +1,5 @@
+let x = 11;
+let z = 9
+
+const experiment = x --> z && z <-- x;
+console.log(experiment);
---
+++ love_in_py/retail.py
@@ -0,0 +1,16 @@
+'''
+A retail company assigns a $500 store bonus if monthly sales are $100,000 or
+more.
+Additionally, if their sales exeed 125% or more of their monthly goal of
+$90,000 then all employees will reieve a message stating that they will get a
+day off.
+'''
+import random
+
+
+bonus = 500
+monthlyGoal = 90000
+targetPecent = 1.25
+bonusTarget = monthlyGoal * targetPecent
+totalSales
+companyMessage = "Congratulations! You have earned a day off from work!"
