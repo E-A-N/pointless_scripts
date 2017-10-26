07b85ca3df11f507db240db65b98182d5e7227dc
Cyborg_Ean <ean.director@gmail.com>
2017-10-25 20:43:06 -0700

    setting up event signaling

--- javascript/writeFile.js
+++ javascript/writeFile.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
-const fileName = './writeTEST';
+const fileName = './writeTEST.js';
 
-var msg = "Aye we in there! ";
-for(let x = 0; x < 10; x++){
-    msg += msg + "\n";
-}
+var ray = ['not','once','twice','three times!!'];
+var choice
+var msg = `console.log("Our love is ${numb} as large!")`;
+
 
 fs.writeFile(fileName, msg, 'utf8', (arg) => {
     console.log("Finished!!");
 })
-
-fs.readFile()
+fs.readFile(fileName,'utf8', (err, data) => {
+    console.log(data);
+    console.log("File has been read!!")
+});
