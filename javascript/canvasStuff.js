5ab6a7a1fc71d4b1bed2db78e1cbccfbf9222a27
Cyborg_Ean <ean.director@gmail.com>
2017-12-18 12:51:42 -0800

    handled more field properties in text styles

--- javascript/canvasStuff.js
+++ javascript/canvasStuff.js
@@ -3,21 +3,27 @@ const setupText = (canvas, data) => {
         const context = canvas.getContext("2d");
 
         //iterate through data and apply formatted text to textMsg
-        let textMsg
+        let textMsg;
         data.args.forEach( (format, index) => {
-            textMsg = data.text.replace( `%${index + 1}`, format);
+            textMsg = index === 0
+                ? data.text.replace( `%${index + 1}`, format)
+                : textMsg.replace( `%${index + 1}`, format);
         });
+        const concatStyles = (combination , style) => (combination += ` ${style}`);
+        const textStyles = data.fontProperties.reduce(concatStyles);
 
         //Handle styling
+        context.font = textStyles;
         context.strokeStyle = data.strokeStyle;
         context.fillStyle = data.fillStyle;
         context.lineWidth = data.lineWidth;
+        context.textAlign = data.textAlign;
 
         const textRender = [textMsg, data.x, data.y];
         if (data.stroked) context.strokeText(...textRender);
         context.fillText(...textRender);
 
-        //return context;
+        return context;
 };
 
 const pJson = {
@@ -25,12 +31,21 @@ const pJson = {
     stroked: true,
     fillStyle: "white",
     lineWidth: 3,
-    font: "900 36px Arial",
+    fontProperties: ["normal", "10px", "Arial"],
     textAlign: "right",
-    x: 75,
+    x: 250,
     y: 75,
-    text: "Herro Robo, dis is a test!! <3 %1%2...%3",
+    text: "Herro World, %1 is One, %2 is Two, %3 is Three",
     args: ["Uno", "Dos", "Thres!!"],
+    playerInfo: {
+        name: "Ean Moore",
+        id: 1363528553768175,
+        userFriends: [{id: 0, name: "Friends Name!!", photoURL:"www.imgloc.com"}], //array of objects
+        score: 0,
+        playerImage: {}, //image that's apart of the DOM
+        url: "www.imagelocation.com", //url location of image
+
+    },
 }
 
 console.log("body loaded!! <3")
