226f4727c3c20e0977d06203434a927cbc837c26
Cyborg_Ean <ean.director@gmail.com>
2017-12-19 10:16:39 -0800

    restructured json, code needs to be adapted

--- javascript/canvasStuff.js
+++ javascript/canvasStuff.js
@@ -60,57 +60,68 @@ const renderImage = (canvas, data) => {
     return context;
 }
 
-const pJson = {
-    textInfo: {
-        strokeStyle: "black",
-        stroked: true,
-        fillStyle: "white",
-        lineWidth: 4,
-        fontProperties: ["normal", "24px", "Arial"], //keep this order
-        textAlign: "right",
-        x: 275,
-        y: 75,
-        text: "Can you beat my score?",
-        score: 9999999,
-    },
+const model = {
     fbInfo: {
         update: {
             action: 'CUSTOM',
-            template: "beat_my_score",
-            cta: "Play Dragonz Rising",
-            strategy: "IMMEDIATE",
-            notification: "NO_PUSH",
+            template: 'beat_my_score',
+            cta: 'Play Dragonz Rising',
+            strategy: 'IMMEDIATE',
+            notification: 'NO_PUSH'
+       },
+       share: {
+            intent: 'REQUEST',
+            text: 'Can you beat my score?'
+       }
+    },
+    data: [
+        {type: 'playerInfo',
+            playerImage: [Object],
+            url: 'www.imagelocation.com'
         },
-        share: {
-            intent: "REQUEST",
-            text: "Can you beat my score?",
+        {type: 'imageInfo',
+            src: 'imagelocation.com',
+            dx: 0,
+            dy: 0,
+            destinationWidth: 250,
+            destinationHeight: 250,
+            subX: 0,
+            subY: 0,
+            subWidth: 250,
+            subHeight: 250
         },
-    },
-    imageInfo : {
-        //src: "canvasStuff.jpg", //TODO: test a real image location
-        src: "https://www.welivesecurity.com/wp-content/uploads/2017/10/cybersecurity_month-1-623x432.jpg",
-        dx: 0,
-        dy: 0,
-        destinationWidth: 250,
-        destinationHeight: 250,
-        subX: 0,
-        subY: 0,
-        subWidth: 250,
-        subHeight: 250,
-    },
-    playerInfo: {
-        name: "Ean Moore",
-        id: 1363528553768175,
-        userFriends: [{id: 0, name: "Friends Name!!", photoURL:"www.imgloc.com"}], //array of objects
-        score: 0,
-        playerImage: {
-            width: 400,
-            height: 400}, //image that's apart of the DOM
-        url: "www.imagelocation.com", //url location of image
-    },
+        {type: 'textInfo',
+            strokeStyle: 'black',
+            stroked: true,
+            fillStyle: 'white',
+            lineWidth: 4,
+            fontProperties: [Array],
+            textAlign: 'right',
+            x: 275,
+            y: 75,
+            text: 'Can you beat my score?'
+        },
+        {type: 'textInfo',
+            strokeStyle: 'black',
+            stroked: true,
+            fillStyle: 'white',
+            lineWidth: 4,
+            fontProperties: [Array],
+            textAlign: 'right',
+            x: 275,
+            y: 75,
+            text: '9999999'
+        },
+        { type: 'playerInfo',
+           playerImage: [Object],
+           url: 'www.imagelocation.com'
+        }
+    ]
 }
+
+
 
 console.log("body loaded!! <3")
 
-const imgCtx = renderImage(canvas, pJson);
-const ctx = renderText(canvas, pJson);
+const imgCtx = renderImage(canvas, model);
+const ctx = renderText(canvas, model);
