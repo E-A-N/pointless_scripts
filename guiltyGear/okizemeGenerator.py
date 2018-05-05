6d367680d21158938def6fcb76a084c829b35542
Cyborg_Ean <ean.director@gmail.com>
2018-05-05 14:21:58 -0700

    adding basic normal data

--- guiltyGear/okizemeGenerator.py
+++ guiltyGear/okizemeGenerator.py
@@ -1,8 +1,49 @@
+groundNormals = [
+    "5p",
+    "2p",
+    "6p",
+    "5k",
+    "2k",
+    "6k",
+    "2S",
+    "5S",
+    "c.S",
+    "2h",
+    "5h",
+    "6h",
+    "5d",
+    "2d"
+]
+
+aerialNormals = [
+    "j.p",
+    "j.k",
+    "j.s",
+    "j.h",
+    "j.d"
+]
+
+
+#NOTE test other aerial formations for oki that's not j.D ball
 basicAerialOkis = [
-    "j.P K-Ball Oki",
-    "j.K K-Ball Oki",
-    "j.H K-Ball Oki",
-    "j.P j.D-Ball Oki",
-    "j.K j.D-Ball Oki",
-    "j.H j.D-Ball Oki"
+    "j.P K-Ball",
+    "j.K K-Ball",
+    "j.S K-Ball",
+    "j.H K-Ball",
+    "j.P j.D-Ball",
+    "j.K j.D-Ball",
+    "j.H j.D-Ball",
+    "madstruggle j.D-Ball"
+]
+
+basicGroundOkis = [
+    "s.Carcass",
+    "h.Carcass",
+    "s.Stinger",
+    "h.Stinger"
+    "p.Ball"
+]
+
+pBallHIts = [
+    ""
 ]
