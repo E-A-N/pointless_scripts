71750ffd208420f525202eaed29dee57cbcd1ea3
Cyborg_Ean <ean.director@gmail.com>
2018-05-05 14:31:16 -0700

    seperated normals into their own module

---
+++ guiltyGear/moveData.py
@@ -0,0 +1,37 @@
+groundNormals = [
+    "5p",
+    "2p",
+    "6p",
+    "5k",
+    "2k",
+    "6k",
+    "2S",
+    "5S",
+    "c.S",
+    "2h",
+    "5h",
+    "6h",
+    "5d",
+    "2d"
+]
+
+aerialNormals = [
+    "j.p",
+    "j.k",
+    "j.s",
+    "j.h",
+    "j.d"
+]
+
+groundSpecials = [
+    "carcass raid",
+    "stinger aim",
+    "tk madstruggle",
+    "dhm"
+    "ball"
+]
+
+aerialSpecials = [
+    "madstruggle",
+    "ball"
+]
--- guiltyGear/okizemeGenerator.py
+++ guiltyGear/okizemeGenerator.py
@@ -1,27 +1,3 @@
-groundNormals = [
-    "5p",
-    "2p",
-    "6p",
-    "5k",
-    "2k",
-    "6k",
-    "2S",
-    "5S",
-    "c.S",
-    "2h",
-    "5h",
-    "6h",
-    "5d",
-    "2d"
-]
-
-aerialNormals = [
-    "j.p",
-    "j.k",
-    "j.s",
-    "j.h",
-    "j.d"
-]
 
 
 #NOTE test other aerial formations for oki that's not j.D ball
