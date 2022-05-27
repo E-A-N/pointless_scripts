4805afa7c5d4ed8e86b1a461126109bf4972d585
Cyborg_Ean <ean.director@gmail.com>
2022-05-26 19:05:34 -0700

    Update app.py

--- python/app.py
+++ python/app.py
@@ -1,20 +1,36 @@
-from flask import Flask, render_template
+import pandas as pd 
+import re
 
-app = Flask(__name__)
+Regex_Searcher = lambda title: re.search(r'\((\d{4})\)', title) 
+Increment      = lambda var : var + 1
+Data_Pointer = 'data/movies.csv'
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return  render_template('index.html')
+def Ternary(condition, truthValue = True,falseValue = False):
+    if condition:
+      result = truthValue
+    else:
+      result = falseValue
+    return result
+    
+def Get_Title_Year(title):
+    year_regex = Regex_Searcher(title)
+    if year_regex:
+        return year_regex.group(1)
+    else:
+        return None
 
-#Hello World in Phaser
-@app.route('/hellophaser')
-def hellophaser():
-    return render_template('phase.html')
+def Get_Count_Year(year):
+    movie_titles = pd.read_csv(Data_Pointer)['title']
+    total = 0
+    for title in movie_titles:
+        temp = Get_Title_Year(title)
+        if  temp != None:
+            total = Ternary(int(temp) == year, Increment(total), total)
+    return total
 
-@app.route('/monkeys')
-def monk():
-    return 'Monkey D Luffy!! <3'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+def Genre_Iteration(genre):
+    genres = pd.read_csv(Data_Pointer)['genres']
+    iteration = 0
+    for item in genres:
+        iteration = Ternary(genre in item, Increment(iteration), iteration)
+    return iteration
